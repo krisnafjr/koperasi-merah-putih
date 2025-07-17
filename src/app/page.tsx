56ad257088077909918243328895e10e2c899c46
mHZulvanA <144695581+MuhammadZulvanAditya@users.noreply.github.com>
2025-07-17 23:01:28 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,12 @@ import LatestNews from "@/components/LatestNews";
 import Testimonial from "@/components/Testimonial";
 import PotensiDesa from "@/components/PotensiDesa"; // <-- 1. Impor komponen baru
 import CTA from "@/components/CTA";
+import { pendiriQuery } from "@/sanity/lib/pendiri";
+import { client } from "@/sanity/lib/sanityClient";
+import PendiriGrid from "@/components/PendiriGrid";
 
 export default async function HomePage() {
+   const data = await client.fetch(pendiriQuery);
   return (
     <main>
       <Navbar />
@@ -20,6 +24,10 @@ export default async function HomePage() {
       <FeaturedProducts />
       <LatestNews />
       <Testimonial />
+      {/* Section: Struktural Pendiri */}
+      <section className="py-12 px-4 md:px-10 bg-white">
+        <PendiriGrid data={data} />
+      </section>
       <PotensiDesa /> {/* <-- 2. Tambahkan di sini */}
       <CTA />
     </main>
